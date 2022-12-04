402ffc3ac8055a05bcc66be0bb561592bbd9da88
Mikel Zubillaga Areitio <mikel.zubillaga.areitio@gmail.com>
2022-12-04 16:08:48 +0100

    Translatean arguments eta triggers gehituta

--- translate.py
+++ translate.py
@@ -1,6 +1,7 @@
 import os
 import json
 import shutil
+import copy as cp
 
 def main():
     directoryR = 'MEE'
@@ -24,12 +25,15 @@ def translate(filenameR, filenameW):
     for line in jsonR:
         data = json.loads(line)
         newdata["tokens"] = data["tokens"]
-        newdata["labels"] = ["O" for i in range(len(data["tokens"]))]
+        newdata["entities"] = ["O" for i in range(len(data["tokens"]))]
+        newdata["triggers"] = ["O" for i in range(len(data["tokens"]))]
+        newdata["arguments"] = []
         start, end = getStartEnd(data["tokens"])
+        #Entities extraction
         for label in data["entities"]:
             startPos = data["entities"][label]["start"]
             endPos = data["entities"][label]["end"]
-            if ';' in endPos: #?¿
+            if ';' in endPos:
                 endPos = endPos.split(";")[1]
             for i in range(0,len(start)):
                 if start[i] > int(startPos):
@@ -39,9 +43,56 @@ def translate(filenameR, filenameW):
                 if end[i] >= int(endPos):
                     amaiera = i
                     break
-            newdata["labels"][hasiera] = "B-" + data["entities"][label]["type"]
+            newdata["entities"][hasiera] = "B-" + data["entities"][label]["type"]
             for i in range(hasiera + 1,amaiera + 1):
-                newdata["labels"][i] = "I-" + data["entities"][label]["type"]
+                newdata["entities"][i] = "I-" + data["entities"][label]["type"]
+        #Triggers Extraction
+        for label in data["triggers"]:
+            startPos = data["triggers"][label]["start"]
+            endPos = data["triggers"][label]["end"]
+            if ';' in endPos:
+                endPos = endPos.split(";")[1]
+            for i in range(0, len(start)):
+                if start[i] > int(startPos):
+                    hasiera = i - 1
+                    break
+            for i in range(hasiera, len(end)):
+                if end[i] >= int(endPos):
+                    amaiera = i
+                    break
+            newdata["triggers"][hasiera] = "B-" + data["triggers"][label]["type"]
+            for i in range(hasiera + 1, amaiera + 1):
+                newdata["triggers"][i] = "I-" + data["triggers"][label]["type"]
+        #Arguments Extraction
+        for label in data["arguments"]:
+            startPosT = data["triggers"][label["trigger"]]["start"]
+            endPosT = data["triggers"][label["trigger"]]["end"]
+            startPosA = data["entities"][label["argument"]]["start"]
+            endPosA = data["entities"][label["argument"]]["end"]
+            if ';' in endPosT:
+                endPosT = endPosT.split(";")[1]
+            for i in range(0, len(start)):
+                if start[i] > int(startPosT):
+                    hasieraT = i - 1
+                    break
+            for i in range(hasieraT, len(end)):
+                if end[i] >= int(endPosT):
+                    amaieraT = i
+                    break
+            if ';' in endPosA:
+                endPosA = endPosA.split(";")[1]
+            for i in range(0, len(start)):
+                if start[i] > int(startPosA):
+                    hasieraA = i - 1
+                    break
+            for i in range(hasieraA, len(end)):
+                if end[i] >= int(endPosA):
+                    amaieraA = i
+                    break
+            newdict = label
+            newdict["trigger"] = [str(i) for i in range(hasieraT,amaieraT+1)]
+            newdict["argument"] = [str(i) for i in range(hasieraA, amaieraA + 1)]
+            newdata["arguments"].append(cp.deepcopy(newdict))
         write_string = json.dumps(newdata,ensure_ascii=False)
         jsonW.write(write_string + '\n')
     jsonR.close()
