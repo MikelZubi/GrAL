2ed661e87321a1f42e332d666039c3b4d14449b4
Mikel Zubillaga <mikel.zubillaga.areitio@gmail.com>
2022-12-27 23:10:56 +0100

    Trainerrean argument sartuta

--- train.sh
+++ train.sh
@@ -8,9 +8,12 @@ do
         --do_train \
         --do_eval \
         --do_predict \
-        --evaluation_strategy epoch \
+        --evaluation_strategy steps \
         --num_train_epochs 4.0 \
         --weight_decay 0.001 \
+        --load_best_model_at_end True \
+        --save_strategy steps \
+        --eval_steps 500 \
         --train_file MEE_BIO/$language/train.json \
         --label_column_name labels \
         --validation_file MEE_BIO/$language/dev.json \
@@ -23,15 +26,37 @@ do
         --do_train \
         --do_eval \
         --do_predict \
-        --evaluation_strategy epoch \
+        --evaluation_strategy steps \
         --num_train_epochs 4.0 \
         --weight_decay 0.001 \
+        --load_best_model_at_end True \
+        --save_strategy steps \
+        --eval_steps 500 \
         --train_file MEE_BIO/$language/train.json \
         --label_column_name triggers \
         --validation_file MEE_BIO/$language/dev.json \
         --test_file MEE_BIO/$language/test.json \
         --output_dir Models/$language/triggers \
         --overwrite_output_dir \
+    
+    python run_ner.py \
+        --model_name_or_path xlm-roberta-base \
+        --do_train \
+        --do_eval \
+        --do_predict \
+        --evaluation_strategy steps \
+        --num_train_epochs 4.0 \
+        --weight_decay 0.001 \
+        --load_best_model_at_end True \
+        --save_strategy steps \
+        --eval_steps 500 \
+        --train_file MEE_BIO/$language/train_arg.json \
+        --label_column_name arguments \
+        --validation_file MEE_BIO/$language/dev_arg.json \
+        --test_file MEE_BIO/$language/test_arg.json \
+        --output_dir Models/$language/arguments \
+        --overwrite_output_dir \
+    
 
 done
 echo "Bukatuta"
